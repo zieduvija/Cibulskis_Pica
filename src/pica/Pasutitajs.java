d1671d474591fa3f173beb3d3b885fec438ffe8e
zieduvija <kaspars.cibulskis@gmail.com>
2023-03-20 15:44:27 +0200

    Izveidotas Picas un Pasūtītaja klases

---
+++ src/pica/Pasutitajs.java
@@ -0,0 +1,18 @@
+package pica;
+
+public class Pasutitajs {
+    String vards;
+    String uzvards;
+    String telefonaNumurs;
+    String adrese;
+    String apmakasasVeids;
+
+    Pasutitajs(String vards, String uzvards, String telefonaNumurs, String adrese, String apmakasasVeids) {
+        this.vards = vards;
+        this.uzvards = uzvards;
+        this.telefonaNumurs = telefonaNumurs;
+        this.adrese = adrese;
+        this.apmakasasVeids = apmakasasVeids;
+    }
+
+}
---
+++ src/pica/Pica.java
@@ -0,0 +1,20 @@
+package pica;
+
+public class Pica {
+	String izmers;
+    String miklasVeids;
+    String merce;
+    String siers;
+    String gala;
+    String[] piedevas;
+
+    Pica(String izmers, String miklasVeids, String merce,String siers, String gala, String[] piedevas) {
+        this.izmers = izmers;
+        this.miklasVeids = miklasVeids;
+        this.merce = merce;
+        this.siers = siers;
+        this.gala = gala;
+        this.piedevas = piedevas;
+    }
+
+}
